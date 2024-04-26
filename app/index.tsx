fad11d1ccde1d27c8f277144e2ce73cce49cf257
Jose Andrés Mejías Rojas <jose.andresmr@hotmail.com>
2024-04-26 13:41:10 -0600

    Add react query

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,33 @@
-import { StatusBar } from "expo-status-bar";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import firestore from "@react-native-firebase/firestore";
 import * as React from "react";
 import { getFirebaseClient } from "./firebase-client";
+import { useQuery } from "@tanstack/react-query";
 
 type Student = {
   FirstName: string;
   LastName: string;
 };
 
+const getStudents = async () => {
+  const firebaseApp = await getFirebaseClient();
+  const elements = await firestore(firebaseApp).collection("Students").get();
+
+  const result = [] as Student[];
+  elements.forEach((student) => result.push(student.data() as Student));
+  return result;
+};
+
 export default function Page() {
-  const [students, setStudents] = React.useState([] as Student[]);
-
-  React.useEffect(() => {
-    async function getData() {
-      try {
-        const firebaseApp = await getFirebaseClient();
-        const result = [] as Student[];
-        const elements = await firestore(firebaseApp)
-          .collection("Students")
-          .get();
-        elements.forEach((student) => result.push(student.data() as Student));
-        setStudents(result);
-      } catch (error) {
-        console.error("Error getting Firestore", error);
-      }
-    }
-
-    void getData();
-  }, []);
+  const queryStudents = useQuery({
+    queryKey: ["get-students"],
+    queryFn: getStudents,
+  });
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={students}
+        data={queryStudents.data}
         renderItem={(info) => (
           <Text>{`${info.item.FirstName} ${info.item.LastName}`}</Text>
         )}
